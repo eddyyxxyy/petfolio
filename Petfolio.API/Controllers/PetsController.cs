42f7c216bf292acdfd0419ec9d59697c2161a5cc
Edson Pimenta <dev.edsonpimenta@gmail.com>
2025-06-02 18:11:53 -0300

    feat(endpoint): adds Pet update endpoint and renames register/update request DTO

--- Petfolio.API/Controllers/PetsController.cs
+++ Petfolio.API/Controllers/PetsController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Petfolio.Application.UseCases.Pets.Register;
+using Petfolio.Application.UseCases.Pets.Update;
 using Petfolio.Communication.Requests;
 using Petfolio.Communication.Responses;
 
@@ -14,10 +15,25 @@ public class PetsController : ControllerBase
         typeof(ResponseRegisterPetJson),
         StatusCodes.Status201Created
     )]
-    public IActionResult Register([FromBody] RequestRegisterPetJson request)
+    public IActionResult Register([FromBody] RequestPetJson request)
     {
         var response = new RegisterPetUseCase().Execute(request);
 
         return Created(string.Empty, response);
+    }
+
+    [HttpPut]
+    [Route("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public IActionResult Update(
+        [FromRoute] int id,
+        [FromBody] RequestPetJson request
+    )
+    {
+        var useCase = new UpdatePetUseCase();
+
+        useCase.Execute(id, request);
+
+        return NoContent();
     }
 }
